7057ec05cc5a28118f7dc0a218f6fd3c18e98d84
Aadi Rai <aadi.rai2008@gmail.com>
2025-05-26 09:40:31 +1000

    Move evaluation to after variation

--- src/evolve.py
+++ src/evolve.py
@@ -5,14 +5,10 @@ def evolve(toolbox, pop, pc, pm, num_gen):
     pop_size = len(pop)
 
     for _ in range(0, num_gen):
-        for ind in pop:
-            if not ind.fitness.is_valid():
-                ind.fitness.values = toolbox.evaluate(ind)
-
         offspring = toolbox.select(pop, pop_size)
         pop = algorithms.var_and(toolbox, offspring, pc, pm)
 
-    for ind in pop:
-            if not ind.fitness.is_valid():
-                ind.fitness.values = toolbox.evaluate(ind)
+        for ind in pop:
+                if not ind.fitness.is_valid():
+                    ind.fitness.values = toolbox.evaluate(ind)
     return pop
